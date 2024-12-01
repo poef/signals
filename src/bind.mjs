227be1522b4aeb55609327ef4cec6af69ec78a45
Auke van Slooten <auke@muze.nl>
2024-12-01 15:18:31 +0100

    added self healing capabilities of array / template rendering if dom has changed, on update the dom is merged into a correct state again also added decodeURIComponent to path parts, so you can encode . and " in property names

--- src/bind.mjs
+++ src/bind.mjs
@@ -44,17 +44,17 @@ export function bind(options) {
         }
     }
 
-	const applyTemplate = (el, template, list, index) => {
+	const applyTemplate = (path, template, list, index) => {
 	    let clone = template.content.cloneNode(true)
 	    if (clone.children.length>1) {
 	        throw new Error('template must contain a single root node', { cause: template })
 	    }
 	    const bindings = clone.querySelectorAll('['+options.attribute+']')
 	    for (let binding of bindings) {
-	        binding.dataset.bind = el.dataset.bind+'.'+index+'.'+binding.dataset.bind
+	        binding.dataset.bind = path+'.'+index+'.'+binding.dataset.bind
 	    }
 	    clone.children[0].setAttribute(options.attribute+'-key',index)
-	    el.appendChild(clone)
+	    return clone
 	}
 
 	const render = (el, root, path) => {
@@ -63,31 +63,58 @@ export function bind(options) {
 	    throttledEffect(() => { // FIXME: throttledEffect runs once too much (extra time at the end)
 	        const value = getValueByPath(root, path)
 	        if (Array.isArray(value) && template) {
+	        	let items = el.querySelectorAll(':scope > [data-bind-key]')
+	        	// do single merge strategy for now, in future calculate optimal merge strategy from a number
+	        	// now just do a delete if a key <= last key, insert if a key >= last key
+	        	let lastKey = 0
+	        	for (let item of items) {
+	        		if (item.dataset.bindKey>lastKey) {
+	        			// insert before
+	        			el.insertBefore(applyTemplate(path, template, value, lastKey), item)
+	        		} else if (item.dataset.bindKey<lastKey) {
+	        			// remove this
+	        			item.remove()
+	        		} else {
+	        			// check that all data-bind params start with current json path or a '#', otherwise replaceChild
+	        			let bindings = item.querySelectorAll(':scope :not([data-bind-key]) [data-bind]')
+	        			let needsReplacement = Array.from(bindings).find(b => b.dataset.bind.substr(0, path.length)!==path)
+	        			if (needsReplacement) {
+	        				el.replaceChild(item, applyTemplate(path, template, value, lastKey))
+	        			}
+	        		}
+	        		lastKey++
+	        		if (lastKey>=value.length) {
+	        			break
+	        		}
+	        	}
+	        	items = el.querySelectorAll(':scope > [data-bind-key]')
+	        	let length = items.length
 	            if (length > value.length) {
 	                while (length > value.length) {
-	                    let child = el.querySelector(':scope > :nth-child('+(length+1)+')')
-	                    child.remove()
+	                    let child = el.querySelector(':scope > :nth-child('+(length+1)+')') //FIXME: assumes 1 template element
+	                    child?.remove()
 	                    length--
 	                }
 	            } else if (length < value.length ) {
 	                while (length < value.length) {
+	                    el.appendChild(applyTemplate(path, template, value, length))
 	                    length++
-	                    applyTemplate(el, template, value, length-1)
 	                }
 	            }
-	            length = value.length
 	        } else if (el.tagName=='INPUT') {
 	            if (el.type=='checkbox' || el.type=='radio') {
-	                if (el.value == value) {
+	                if (el.value == ''+value) {
 	                    el.checked = true
 	                } else {
 	                    el.checked = false
 	                }
-	            } else {
-	                el.value = value
+	            } else if (el.value != ''+value) {
+	                el.value = ''+value
 	            }
 	        } else if (el.tagName=='BUTTON') {
-	            el.value = value
+	        	if (el.value!=''+value) {
+		            el.value = ''+value
+		        }
 	        } else if (el.tagName=='SELECT') {
 	            if (el.multiple) {
 	                if (Array.isArray(value)) {
@@ -106,7 +133,9 @@ export function bind(options) {
 	                }
 	            }
 	        } else {
-	            el.innerHTML = value
+	        	if (el.innerHTML != ''+value) {
+		            el.innerHTML = ''+value
+		        }
 	        }
 	    }, 100)
 	}
@@ -127,6 +156,7 @@ function getValueByPath(root, path) {
     let part, prevPart;
     while (parts.length && curr) {
         part = parts.shift()
+        part = decodeURIComponent(part)
         if (part=='#key') {
             return prevPart
         } else if (part=='#value') {
