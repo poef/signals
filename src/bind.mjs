70330d10f4688a47a66b0b14c18589b6f97beae5
Auke van Slooten <auke@muze.nl>
2024-12-15 15:19:38 +0100

    refactoring bind to add transformers

--- src/bind.mjs
+++ src/bind.mjs
@@ -1,10 +1,11 @@
-import { signal, effect, throttledEffect } from './signals.mjs'
+import { throttledEffect } from './signals.mjs'
 
 export function bind(options)
 {
     const defaultOptions = {
         container: document.body,
-        attribute: 'data-bind'
+        attribute: 'data-bind',
+        transformers: []
     }
     if (!options?.root) {
         throw new Error('bind needs at least options.root set')
@@ -16,8 +17,8 @@ export function bind(options)
             if (change.type=="childList" && change.addedNodes) {
                 for (let node of change.addedNodes) {
                     if (node instanceof HTMLElement) {
-                        let bindings = Array.from(node.querySelectorAll('[data-bind]'))
-                        if (node.matches('[data-bind')) {
+                        let bindings = Array.from(node.querySelectorAll(`[${options.attribute}]`))
+                        if (node.matches(`[${options.attribute}]`)) {
                             bindings.unshift(node)
                         }
                         if (bindings.length) {
@@ -68,142 +69,14 @@ export function bind(options)
     }
 
     const render = (el, root) => {
-        let template = el.querySelector('template')
-        throttledEffect(() => { // FIXME: throttledEffect runs once too much (extra time at the end)
+        throttledEffect(() => {
+            const template = el.querySelector('template')
             const path = getBindingPath(el)
-            const value = getValueByPath(root, path)
-            // TODO: add support for object as value, both with/without toString methods
-            // should allow to iterate over object entries and render them with a template
-            // TODO: support multiple templates and a way to select the correct one per entry
-            if (Array.isArray(value) && template) {
-                let items = el.querySelectorAll(':scope > [data-bind-key]')
-                // do single merge strategy for now, in future calculate optimal merge strategy from a number
-                // now just do a delete if a key <= last key, insert if a key >= last key
-                // FIXME: there are still situations where dom data-binding is not correctly fixed after a dom change
-                // see the kanban demo
-                let lastKey = 0
-                for (let item of items) {
-                    if (item.dataset.bindKey>lastKey) {
-                        // insert before
-                        el.insertBefore(applyTemplate(path, template, value, lastKey), item)
-                    } else if (item.dataset.bindKey<lastKey) {
-                        // remove this
-                        item.remove()
-                    } else {
-                        // check that all data-bind params start with current json path or a '#', otherwise replaceChild
-                        let bindings = Array.from(item.querySelectorAll('[data-bind]'))
-                        if (item.matches('[data-bind]')) {
-                            bindings.unshift(item)
-                        }
-                        let needsReplacement = bindings.find(b => {
-                            return (b.dataset.bind.substr(0,5)!=='#root' && b.dataset.bind.substr(0, path.length)!==path)
-                        })
-                        if (needsReplacement) {
-                            el.replaceChild(applyTemplate(path, template, value, lastKey), item)
-                        }
-                    }
-                    lastKey++
-                    if (lastKey>=value.length) {
-                        break
-                    }
-                }
-                items = el.querySelectorAll(':scope > [data-bind-key]')
-                let length = items.length
-                if (length > value.length) {
-                    while (length > value.length) {
-                        let child = el.querySelector(':scope > :nth-child('+(length+1)+')') //FIXME: assumes 1 template element
-                        child?.remove()
-                        length--
-                    }
-                } else if (length < value.length ) {
-                    while (length < value.length) {
-                        el.appendChild(applyTemplate(path, template, value, length))
-                        length++
-                    }
-                }
-            } else if (value && typeof value == 'object' && template) {
-                let list    = Object.entries(value)
-                let items   = el.querySelectorAll(':scope > [data-bind-key]')
-                let current = 0
-                for (let item of items) {
-                    if (current>=list.length) {
-                        break
-                    }
-                    let key = list[current][0]
-                    current++
-                    let keypath = path+'.'+key
-                    // check that all data-bind params start with current json path or a '#', otherwise replaceChild
-                    let needsReplacement
-                    if (item.dataset?.bind && item.dataset.bind.substr(0, keypath.length)!=keypath) {
-                        needsReplacement=true
-                    } else {
-                        let bindings = Array.from(item.querySelectorAll('[data-bind]'))
-                        needsReplacement = bindings.find(b => {
-                            return (b.dataset.bind.substr(0,5)!=='#root' && b.dataset.bind.substr(0, keypath.length)!==keypath)
-                        })
-                    }
-                    if (needsReplacement) {
-                        el.replaceChild(applyTemplate(path, template, value, key), item)
-                    }
-                }
-                items  = el.querySelectorAll(':scope > [data-bind-key]')
-                let length = items.length
-                if (length>list.length) {
-                    while (length>list.length) {
-                        let child = el.querySelector(':scope > :nth-child('+(length+1)+')') //FIXME: assumes 1 template element
-                        child?.remove()
-                        length--
-                    }
-                } else if (length < list.length) {
-                    while (length < list.length) {
-                        let key = list[length][0]
-                        el.appendChild(applyTemplate(path, template, value, key))
-                        length++
-                    }
-                }
-            } else if (el.tagName=='INPUT') {
-                if (el.type=='checkbox' || el.type=='radio') {
-                    if (el.value == ''+value) {
-                        el.checked = true
-                    } else {
-                        el.checked = false
-                    }
-                } else if (el.value != ''+value) {
-                    el.value = ''+value
-                }
-            } else if (el.tagName=='BUTTON') {
-                if (el.value!=''+value) {
-                    el.value = ''+value
-                }
-            } else if (el.tagName=='SELECT') {
-                if (el.multiple) {
-                    if (Array.isArray(value)) {
-                        for (let option of el.options) {
-                            if (value.indexOf(option.value)===false) {
-                                option.selected = false
-                            } else {
-                                option.selected = true
-                            }
-                        }
-                    }
-                } else {
-                    let option = el.options.find(o => o.value==value)
-                    if (option) {
-                        option.selected = true
-                    }
-                }
-            } else if (el.tagName=='A') {
-                if (value?.innerHTML && el.innerHTML!=''+value.innerHTML) {
-                    el.innerHTML = ''+value.innerHTML
-                }
-                if (value?.href && el.href != ''+value.href) {
-                    el.href = ''+value.href
-                }
-            } else {
-                if (el.innerHTML != ''+value) {
-                    el.innerHTML = ''+value
-                }
+            const value = getValueByPath(root, path) || ''
+            if (!el.dataset.transform || !options.transformers[el.dataset.transform]) {
+                return defaultTransformer.call(el, {template, path, value}, applyTemplate)
             }
+            return options.transformers[el.dataset.transform].call(el, {template, path, value}, applyTemplate)
         }, 100)
     }
     
@@ -214,6 +87,141 @@ export function bind(options)
     var bindings = options.container.querySelectorAll('[data-bind]')
     if (bindings.length) {
         applyBindings(bindings)
+    }
+}
+
+export function defaultTransformer(options, applyTemplate) {
+    const template = options.template
+    const path = options.path
+    const value = options.value
+    // TODO: support multiple templates and a way to select the correct one per entry
+    if (Array.isArray(value) && template) {
+        let items = this.querySelectorAll(':scope > [data-bind-key]')
+        // do single merge strategy for now, in future calculate optimal merge strategy from a number
+        // now just do a delete if a key <= last key, insert if a key >= last key
+        let lastKey = 0
+        for (let item of items) {
+            if (item.dataset.bindKey>lastKey) {
+                // insert before
+                this.insertBefore(applyTemplate(path, template, value, lastKey), item)
+            } else if (item.dataset.bindKey<lastKey) {
+                // remove this
+                item.remove()
+            } else {
+                // check that all data-bind params start with current json path or a '#', otherwise replaceChild
+                let bindings = Array.from(item.querySelectorAll('[data-bind]'))
+                if (item.matches('[data-bind]')) {
+                    bindings.unshift(item)
+                }
+                let needsReplacement = bindings.find(b => {
+                    return (b.dataset.bind.substr(0,5)!=='#root' 
+                        && b.dataset.bind.substr(0, path.length)!==path)
+                })
+                if (needsReplacement) {
+                    this.replaceChild(applyTemplate(path, template, value, lastKey), item)
+                }
+            }
+            lastKey++
+            if (lastKey>=value.length) {
+                break
+            }
+        }
+        items = this.querySelectorAll(':scope > [data-bind-key]')
+        let length = items.length
+        if (length > value.length) {
+            while (length > value.length) {
+                let child = this.querySelector(':scope > :nth-child('+(length+1)+')') //FIXME: assumes 1 template element
+                child?.remove()
+                length--
+            }
+        } else if (length < value.length ) {
+            while (length < value.length) {
+                this.appendChild(applyTemplate(path, template, value, length))
+                length++
+            }
+        }
+    } else if (value && typeof value == 'object' && template) {
+        let list    = Object.entries(value)
+        let items   = this.querySelectorAll(':scope > [data-bind-key]')
+        let current = 0
+        for (let item of items) {
+            if (current>=list.length) {
+                break
+            }
+            let key = list[current][0]
+            current++
+            let keypath = path+'.'+key
+            // check that all data-bind params start with current json path or a '#', otherwise replaceChild
+            let needsReplacement
+            if (item.dataset?.bind && item.dataset.bind.substr(0, keypath.length)!=keypath) {
+                needsReplacement=true
+            } else {
+                let bindings = Array.from(item.querySelectorAll('[data-bind]'))
+                needsReplacement = bindings.find(b => {
+                    return (b.dataset.bind.substr(0,5)!=='#root' && b.dataset.bind.substr(0, keypath.length)!==keypath)
+                })
+            }
+            if (needsReplacement) {
+                this.replaceChild(applyTemplate(path, template, value, key), item)
+            }
+        }
+        items  = this.querySelectorAll(':scope > [data-bind-key]')
+        let length = items.length
+        if (length>list.length) {
+            while (length>list.length) {
+                let child = this.querySelector(':scope > :nth-child('+(length+1)+')') //FIXME: assumes 1 template element
+                child?.remove()
+                length--
+            }
+        } else if (length < list.length) {
+            while (length < list.length) {
+                let key = list[length][0]
+                this.appendChild(applyTemplate(path, template, value, key))
+                length++
+            }
+        }
+    } else if (this.tagName=='INPUT') {
+        if (this.type=='checkbox' || this.type=='radio') {
+            if (this.value == ''+value) {
+                this.checked = true
+            } else {
+                this.checked = false
+            }
+        } else if (this.value != ''+value) {
+            this.value = ''+value
+        }
+    } else if (this.tagName=='BUTTON') {
+        if (this.value!=''+value) {
+            this.value = ''+value
+        }
+    } else if (this.tagName=='SELECT') {
+        if (this.multiple) {
+            if (Array.isArray(value)) {
+                for (let option of this.options) {
+                    if (value.indexOf(option.value)===false) {
+                        option.selected = false
+                    } else {
+                        option.selected = true
+                    }
+                }
+            }
+        } else {
+            let option = this.options.find(o => o.value==value)
+            if (option) {
+                option.selected = true
+            }
+        }
+    } else if (this.tagName=='A') {
+        if (value?.innerHTML && this.innerHTML!=''+value.innerHTML) {
+            this.innerHTML = ''+value.innerHTML
+        }
+        if (value?.href && this.href != ''+value.href) {
+            this.href = ''+value.href
+        }
+    } else {
+        if (this.innerHTML != ''+value) {
+            this.innerHTML = ''+value
+        }
     }
 }
 
